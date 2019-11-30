dcd5c733f231813268490252fdb830e8ecdddfb6
九牛一牧 <34566246+2717346533@users.noreply.github.com>
2019-11-30 12:13:44 +0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -7,9 +7,10 @@ from flask import Flask, render_template
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 import os
+import pymysql
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:123456@127.0.0.1:3306/movie"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:123456@127.0.0.1:3306/movie"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 app.config["SECRET_KEY"] = 'e97bfed3-e185-4a6f-9bb3-5f2f84089614'
 app.config["UP_DIR"] = os.path.join(os.path.abspath(os.path.dirname(__file__)),"static/uploads/")
